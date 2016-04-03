427a749ee78336d3121c134931c0e633be94513b
SONGJUNGWOO <jwsong0617@gmail.com>
2016-04-03 14:00:45 +0900

    ad fc7+fc1000 script and fc6 scripts

---
+++ CaffeNet_scripts/ImageFeature_test_fc6.py
@@ -0,0 +1,68 @@
+caffe_root = '/home/ubuntu/caffe/'
+data_root = '/home/ubuntu/kaggle/raw/'
+h5_root = '/mnt/'
+
+import numpy as np
+import sys
+sys.path.insert(0, caffe_root + 'python')
+
+import caffe
+
+import os
+"""
+if not os.path.isfile(caffe_root + 'models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel'):
+    print("Downloading pre-trained CaffeNet model...")
+    !caffe_root/scripts/download_model_binary.py ../models/bvlc_reference_caffenet
+"""
+## Use GPU
+caffe.set_device(0)
+caffe.set_mode_gpu()
+def extract_features(images, layer = 'fc6'):
+    net = caffe.Net(caffe_root + 'models/bvlc_reference_caffenet/deploy.prototxt',
+                caffe_root + 'models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel',
+                caffe.TEST)
+
+    # input preprocessing: 'data' is the name of the input blob == net.inputs[0]
+    transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
+    transformer.set_transpose('data', (2,0,1))
+    transformer.set_mean('data', np.load(caffe_root + 'python/caffe/imagenet/ilsvrc_2012_mean.npy').mean(1).mean(1)) # mean pixel
+    transformer.set_raw_scale('data', 255)  # the reference model operates on images in [0,255] range instead of [0,1]
+    transformer.set_channel_swap('data', (2,1,0))  # the reference model has channels in BGR order instead of RGB]]
+
+    num_images= len(images)
+    net.blobs['data'].reshape(num_images,3,227,227)
+    net.blobs['data'].data[...] = map(lambda x: transformer.preprocess('data',caffe.io.load_image(x)), images)
+    out = net.forward()
+
+    return net.blobs[layer].data
+
+import h5py
+import pandas as pd
+batch_size = 100
+
+f = h5py.File(h5_root+'test_image_fc6features.h5','w')
+filenames = f.create_dataset('photo_id',(0,), maxshape=(None,),dtype='|S54')
+feature = f.create_dataset('feature',(0,4096), maxshape = (None,4096))
+f.close()
+
+
+test_photos = pd.read_csv(data_root+'test_photo_to_biz.csv')
+test_folder = data_root+'test_photos/test_photos'
+test_images = [os.path.join(test_folder, str(x)+'.jpg') for x in test_photos['photo_id'].unique()]
+num_test = len(test_images)
+print "Number of test images: ", num_test
+
+# Test Images
+for i in range(0, num_test, batch_size):
+    images = test_images[i: min(i+batch_size, num_test)]
+    features = extract_features(images, layer='fc6')
+    num_done = i+features.shape[0]
+
+    f= h5py.File(h5_root+'test_image_fc6features.h5','r+')
+    f['photo_id'].resize((num_done,))
+    f['photo_id'][i: num_done] = np.array(images)
+    f['feature'].resize((num_done,features.shape[1]))
+    f['feature'][i: num_done, :] = features
+    f.close()
+    if num_done%20000==0 or num_done==num_test:
+        print "Test images processed: ", num_done
--- fc7+fc1000.py
+++ fc7+fc1000.py
@@ -1,21 +1,27 @@
-#Uncomment if skip previous train
 import numpy as np
 import pandas as pd
 from sklearn import svm
-from sklearn.preprocessing import label_binarize
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn import preprocessing
 import time
 
 t = time.time()
 
-data_root = 'F:/Yelp_data/ResNet/'
-train_df = pd.read_csv(data_root+"train_biz_fc1000features.csv")
-test_df  = pd.read_csv(data_root+"test_biz_fc1000features.csv")
+data_root = 'F:/Yelp_data/caffenet/'
+data_root_ResNet = 'F:/Yelp_data/ResNet/'
+
+train_df = pd.read_csv(data_root+"train_biz_fc7features.csv")
+test_df  = pd.read_csv(data_root+"test_biz_fc7features.csv")
+train_df_Res = pd.read_csv(data_root_ResNet+"train_biz_fc1000features.csv")
+test_df_Res = pd.read_csv(data_root_ResNet+"test_biz_fc1000features.csv")
 
 y_train = train_df['label'].values
 X_train = train_df['feature vector'].values
 X_test = test_df['feature vector'].values
+#y_train_Res = train_df_Res['label'].values
+X_train_Res = train_df_Res['feature vector'].values
+X_test_Res = test_df_Res['feature vector'].values
 
 def convert_label_to_array(str_label):
     str_label = str_label[1:-1]
@@ -30,22 +36,32 @@ def convert_feature_to_vector(str_feature):
 y_train = np.array([convert_label_to_array(y) for y in train_df['label']])
 X_train = np.array([convert_feature_to_vector(x) for x in train_df['feature vector']])
 X_test = np.array([convert_feature_to_vector(x) for x in test_df['feature vector']])
+#y_train_Res = np.array([convert_label_to_array(y) for y in train_df_Res['label']])
+X_train_Res = np.array([convert_feature_to_vector(x) for x in train_df_Res['feature vector']])
+X_test_Res = np.array([convert_feature_to_vector(x) for x in test_df_Res['feature vector']])
+
+X_train_scaled = preprocessing.scale(X_train)
+X_train_scaled_Res = preprocessing.scale(X_train_Res)
+X_train_scaled_Concat = np.hstack((X_train,X_train_Res))
+X_test_scaled = preprocessing.scale(X_test)
+X_test_scaled_Res = preprocessing.scale(X_test_Res)
+X_test_scaled_Concat = np.hstack((X_test,X_test_Res))
 
 mlb = MultiLabelBinarizer()
 y_train= mlb.fit_transform(y_train)  #Convert list of labels to binary matrix
 
 random_state = np.random.RandomState(0)
 classifier = OneVsRestClassifier(svm.SVC(kernel='linear', probability=True))
-classifier.fit(X_train, y_train)
+classifier.fit(X_train_scaled_Concat, y_train)
 
-y_predict = classifier.predict(X_test)
+y_predict = classifier.predict(X_test_scaled_Concat)
 
 #print list(mlb.classes_)
 y_predict_label = mlb.inverse_transform(y_predict) #Convert binary matrix back to labels
 
 print "Time passed: ", "{0:.1f}".format(time.time()-t), "sec"
 
-test_data_frame  = pd.read_csv(data_root+"test_biz_fc1000features.csv")
+test_data_frame  = pd.read_csv(data_root+"test_biz_fc7features.csv") #fc7features and fc1000features have same business names
 df = pd.DataFrame(columns=['business_id','labels'])
 
 for i in range(len(test_data_frame)):
@@ -54,7 +70,7 @@ for i in range(len(test_data_frame)):
     label = str(label)[1:-1].replace(",", " ")
     df.loc[i] = [str(biz), label]
 
-with open(data_root+"submission_fc1000.csv",'w') as f:
+with open(data_root+"submission_fc7_fc1000.csv",'w') as f:
     df.to_csv(f, index=False)
 
 statistics = pd.DataFrame(columns=[ "attribuite "+str(i) for i in range(9)]+['num_biz'], index = ["biz count", "biz ratio"])
