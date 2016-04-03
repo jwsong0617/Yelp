cd0da3864a55692db34617731954b1aa22985d49
SONGJUNGWOO <jwsong0617@gmail.com>
2016-04-03 21:45:01 +0900

    edit fc6 businessFeature_train_fc6, fc6+fc7+fc1000 scripts

--- fc6+fc7+fc1000_test_normalization_eachSample.py
+++ fc6+fc7+fc1000_test_normalization_eachSample.py
@@ -21,6 +21,9 @@ test_df_Res = pd.read_csv(data_root_ResNet+"test_biz_fc1000features.csv")
 y_train = train_df['label'].values
 X_train = train_df['feature vector'].values
 X_test = test_df['feature vector'].values
+#y_train_fc6 = train_df['label'].values
+X_train_fc6 = train_df_fc6['feature vector'].values
+X_test_fc6 = test_df_fc6['feature vector'].values
 #y_train_Res = train_df_Res['label'].values
 X_train_Res = train_df_Res['feature vector'].values
 X_test_Res = test_df_Res['feature vector'].values
@@ -38,16 +41,20 @@ def convert_feature_to_vector(str_feature):
 y_train = np.array([convert_label_to_array(y) for y in train_df['label']])
 X_train = np.array([convert_feature_to_vector(x) for x in train_df['feature vector']])
 X_test = np.array([convert_feature_to_vector(x) for x in test_df['feature vector']])
+X_train_fc6 = np.array([convert_feature_to_vector(x) for x in train_df_fc6['feature vector']])
+X_test_fc6 = np.array([convert_feature_to_vector(x) for x in test_df_fc6['feature vector']])
 #y_train_Res = np.array([convert_label_to_array(y) for y in train_df_Res['label']])
 X_train_Res = np.array([convert_feature_to_vector(x) for x in train_df_Res['feature vector']])
 X_test_Res = np.array([convert_feature_to_vector(x) for x in test_df_Res['feature vector']])
 
 X_train_scaled = preprocessing.normalize(X_train, norm='l2')
 X_train_scaled_Res = preprocessing.normalize(X_train_Res, norm='l2')
-X_train_scaled_Concat = np.hstack((X_train_scaled,X_train_scaled_Res))
+X_train_scaled_fc6 = preprocessing.normalize(X_train_fc6, norm='l2')
+X_train_scaled_Concat = np.hstack((X_train_scaled,X_train_scaled_Res,X_train_scaled_fc6))
 X_test_scaled = preprocessing.normalize(X_test, norm='l2')
 X_test_scaled_Res = preprocessing.normalize(X_test_Res, norm='l2')
-X_test_scaled_Concat = np.hstack((X_test_scaled,X_test_scaled_Res))
+X_test_scaled_fc6 = preprocessing.normalize(X_test_fc6, norm='l2')
+X_test_scaled_Concat = np.hstack((X_test_scaled,X_test_scaled_Res,X_test_scaled_fc6))
 
 mlb = MultiLabelBinarizer()
 y_train= mlb.fit_transform(y_train)  #Convert list of labels to binary matrix
@@ -72,7 +79,7 @@ for i in range(len(test_data_frame)):
     label = str(label)[1:-1].replace(",", " ")
     df.loc[i] = [str(biz), label]
 
-with open(data_root+"submission_fc7_fc1000_norm.csv",'w') as f:
+with open(data_root+"submission_fc6_fc7_fc1000_norm.csv",'w') as f:
     df.to_csv(f, index=False)
 
 statistics = pd.DataFrame(columns=[ "attribuite "+str(i) for i in range(9)]+['num_biz'], index = ["biz count", "biz ratio"])
